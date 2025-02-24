1e503cca0ab23c745de7ee83cf2f482487a08895
bissaliev <bissaliev21@gmail.com>
2025-02-24 15:54:13 +0400

    Добавил файл конфигураций проекта

---
+++ app/config.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    POSTGRES_DB: str
+    DB_HOST: str
+    DB_PORT: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+
+    model_config = SettingsConfigDict(".env")
+
+    def get_db_postgres_url(self):
+        return f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.POSTGRES_DB}"
+
+    def get_db_sqlite_url(self):
+        return "sqlite:///dbase.db"
+
+
+settings = Settings()
